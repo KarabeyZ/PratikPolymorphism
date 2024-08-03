952cd13865b45d540afdbd5523a5b5e6933b67d6
Zekeriya <Zekeriya@ZEKERIYA>
2024-08-03 14:54:22 +0300

    Add project files.

---
+++ PratikPolymorphism/BaseGeometrikSekil.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PratikPolymorphism
+{
+    //base class
+    public class BaseGeometrikSekil
+    {
+        //türetilmiş sınıfların ulaşması için protected kullandım
+        protected int _genislik { get; set; }
+        protected int _uzunluk { get; set; }
+        public BaseGeometrikSekil(int genislik, int uzunluk)
+        {
+            _genislik = genislik;
+            _uzunluk = uzunluk;
+        }
+        //override yapılacağı için virtual kullanıldı
+        public virtual int AlanHesapla()
+        {
+            int alan = _genislik * _uzunluk;
+            return alan;
+
+        }
+    }
+    //kare sınıfı ve altında constructor bulunuyor
+    public class Kare : BaseGeometrikSekil
+    {
+        public Kare(int kenarUzunluk,int kenarUzunluk2) : base(kenarUzunluk, kenarUzunluk)
+        {
+        }
+    }
+    //dikdörtgen sınıfı ve altında constructor bulunuyor
+    public class Dikdörtgen : BaseGeometrikSekil
+    {
+        public Dikdörtgen(int uzunKenar, int kisaKenar) : base(uzunKenar, kisaKenar)
+        {
+        }
+    }
+    //dikucgen sınıfı ve altında constructor bulunuyor
+    public class DikUcgen : BaseGeometrikSekil
+    {
+        public DikUcgen(int kenarUzunluk, int kenarUzunluk2) : base(kenarUzunluk, kenarUzunluk2)
+        {
+            
+        }
+        //alanhesapla metodu override edilerek alan hesaplama yöntemi değiştirildi
+        public override int AlanHesapla()
+        {
+            int alan = (_genislik * _uzunluk) / 2;
+            return alan;
+        }
+    }
+}
---
+++ PratikPolymorphism/Program.cs
@@ -0,0 +1,10 @@
+﻿using PratikPolymorphism;
+
+Kare kare = new Kare(5,5);
+Console.WriteLine("Karenin alanı: " + kare.AlanHesapla());
+
+Dikdörtgen dikdörtgen = new Dikdörtgen(8, 6);
+Console.WriteLine("Dikdörtgenin alanı: " + dikdörtgen.AlanHesapla());
+
+DikUcgen dikucgen = new DikUcgen(8, 4);
+Console.WriteLine("Diküçgenin alanı: "+ dikucgen.AlanHesapla());
